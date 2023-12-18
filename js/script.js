ac447c21d91c8b094517824936b5f4fce496bd70
Maksym <maxim.tresn@gmail.com>
2023-12-18 14:09:40 -0800

    Adding the second task

--- js/script.js
+++ js/script.js
@@ -26,4 +26,12 @@
 'use strict';
 
 const numberOfFilms = prompt('Сколько фильмов вы уже посмотрели?', '');
-console.log(numberOfFilms);
+// console.log(numberOfFilms);
+
+const personalMovieDB = {
+  count: numberOfFilms,
+  movies: {},
+  actors: {},
+  genres: [],
+  privat: false,
+};
